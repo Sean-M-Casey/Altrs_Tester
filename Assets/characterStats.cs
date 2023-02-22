94912bb2931ff373afe04079b08ec0daf48b6da8
Sean-M-Casey <sean_mcasey@hotmail.com>
2023-02-22 21:44:10 +1100

    Character Creation Addition
    Added Interpersonal and Interests section. Needs feedback to be added properly to Interpersonal and Interests

--- Assets/characterStats.cs
+++ Assets/characterStats.cs
@@ -6,7 +6,8 @@ using MoreMountains.Feedbacks;
 public class characterStats : MonoBehaviour
 {
 
-
+    // Policework Stats //
+    // 
     bool pstat1;
     // true = gut feeling , false = strategic
     bool pstat2;
@@ -17,6 +18,27 @@ public class characterStats : MonoBehaviour
     int looseUnit;
     int byTheBook;
 
+    // Interpersonal Stats // 
+    bool istat1;
+    // true = friendly , false = cold
+    bool istat2;
+    // true = trusting , false = intimidating
+    bool istat3;
+    // true = empathetic , false = contrarian
+    bool aligned2;
+    int goodCop;
+    int badCop;
+
+    // Interests Stats // 
+    bool intstat1;
+    // true = method man , false = integrated 
+    bool intstat2;
+    // true = shredded , false = slender
+    bool intstat3;
+    // true = wrench monkey , false = coded
+    bool aligned3;
+    int meatHead;
+    int hackerMan;
 
     public GameObject[] buttons;
     [SerializeField] GameObject[] alignmentIcons;
@@ -144,9 +166,252 @@ public class characterStats : MonoBehaviour
         AlignmentAssignment();
 
     }
+    // Interpersonal Methods // 
+    public void Friendly()
+    {
+        istat1 = true;
+        buttons[6].GetComponent<Button>().interactable = false;
+        buttons[7].GetComponent<Button>().interactable = true;
+        goodCop++;
+        if (badCop > 0)
+        {
+            badCop--;
+        }
+        if (goodCop == 2)
+        {
+            aligned2 = true;
+        }
+        else
+        {
+            aligned2 = false;
+        }
+        AlignmentAssignment();
+    }
+    public void Cold()
+    {
+        istat1 = false;
+        buttons[6].GetComponent<Button>().interactable = true;
+        buttons[7].GetComponent<Button>().interactable = false;
+        badCop++;
+        if (goodCop > 0)
+        {
+            goodCop --;
+        }
+        if (badCop == 2)
+        {
+            aligned2 = true;
+        }
+        else
+        {
+            aligned2 = false;
+        }
+        AlignmentAssignment();
+    }
+    public void Trusting()
+    {
+        istat2 = true;
+        buttons[8].GetComponent<Button>().interactable = false;
+        buttons[9].GetComponent<Button>().interactable = true;
+        goodCop++;
+        if (badCop > 0)
+        {
+            badCop--;
+        }
+        if (goodCop == 2)
+        {
+            aligned2 = true;
+        }
+        else
+        {
+            aligned2 = false;
+        }
+        AlignmentAssignment();
+    }
+    public void Intimidating()
+    {
+        istat2 = false;
+        buttons[8].GetComponent<Button>().interactable = true;
+        buttons[9].GetComponent<Button>().interactable = false;
+        badCop++;
+        if (goodCop > 0)
+        {
+            goodCop--;
+        }
+        if (badCop == 2)
+        {
+            aligned2 = true;
+        }
+        else
+        {
+            aligned2 = false;
+        }
+        AlignmentAssignment();
+    }
+    public void Empathetic()
+    {
+        istat3 = true;
+        buttons[10].GetComponent<Button>().interactable = false;
+        buttons[11].GetComponent<Button>().interactable = true;
+        goodCop++;
+        if (badCop > 0)
+        {
+            badCop--;
+        }
+        if (goodCop == 2)
+        {
+            aligned2 = true;
+        }
+        else
+        {
+            aligned2 = false;
+        }
+        AlignmentAssignment();
+    }
+    public void Contrarian()
+    {
+        istat3 = false;
+        buttons[10].GetComponent<Button>().interactable = true;
+        buttons[11].GetComponent<Button>().interactable = false;
+        badCop++;
+        if (goodCop > 0)
+        {
+            goodCop--;
+        }
+        if (badCop == 2)
+        {
+            aligned2 = true;
+        }
+        else
+        {
+            aligned2 = false;
+        }
+        AlignmentAssignment();
+    }
 
+    // INTERESTS // 
+    public void MethodMan()
+    {
+        intstat1 = true;
+        buttons[12].GetComponent<Button>().interactable = false;
+        buttons[13].GetComponent<Button>().interactable = true;
+        meatHead++;
+        if (hackerMan > 0)
+        {
+            hackerMan--;
+        }
+        if (meatHead == 2)
+        {
+            aligned3 = true;
+        }
+        else
+        {
+            aligned3 = false;
+        }
+        AlignmentAssignment();
+    }
+    public void Integrated()
+    {
+        intstat3 = false;
+        buttons[12].GetComponent<Button>().interactable = true;
+        buttons[13].GetComponent<Button>().interactable = false;
+        hackerMan++;
+        if (meatHead > 0)
+        {
+            meatHead--;
+        }
+        if (hackerMan == 2)
+        {
+            aligned3 = true;
+        }
+        else
+        {
+            aligned3 = false;
+        }
+        AlignmentAssignment();
+    }
+    public void Shredded()
+    {
+        intstat1 = true;
+        buttons[14].GetComponent<Button>().interactable = false;
+        buttons[15].GetComponent<Button>().interactable = true;
+        meatHead++;
+        if (hackerMan > 0)
+        {
+            hackerMan--;
+        }
+        if (meatHead == 2)
+        {
+            aligned3 = true;
+        }
+        else
+        {
+            aligned3 = false;
+        }
+        AlignmentAssignment();
+    }
+    public void Slender()
+    {
+        intstat3 = false;
+        buttons[14].GetComponent<Button>().interactable = true;
+        buttons[15].GetComponent<Button>().interactable = false;
+        hackerMan++;
+        if (meatHead > 0)
+        {
+            meatHead--;
+        }
+        if (hackerMan == 2)
+        {
+            aligned3 = true;
+        }
+        else
+        {
+            aligned3 = false;
+        }
+        AlignmentAssignment();
+    }
+    public void WrenchMonkey()
+    {
+        intstat1 = true;
+        buttons[16].GetComponent<Button>().interactable = false;
+        buttons[17].GetComponent<Button>().interactable = true;
+        meatHead++;
+        if (hackerMan > 0)
+        {
+            hackerMan--;
+        }
+        if (meatHead == 2)
+        {
+            aligned3 = true;
+        }
+        else
+        {
+            aligned3 = false;
+        }
+        AlignmentAssignment();
+    }
+    public void Coded()
+    {
+        intstat3 = false;
+        buttons[16].GetComponent<Button>().interactable = true;
+        buttons[17].GetComponent<Button>().interactable = false;
+        hackerMan++;
+        if (meatHead > 0)
+        {
+            meatHead--;
+        }
+        if (hackerMan == 2)
+        {
+            aligned3 = true;
+        }
+        else
+        {
+            aligned3 = false;
+        }
+        AlignmentAssignment();
+    }
     public void AlignmentAssignment()
     {
+        // Loose Unit / By the Book Alignments
         if (looseUnit >= 2 && byTheBook <=1 && aligned1 == true)
         {
             alignmentIcons[0].SetActive(true);
@@ -161,10 +426,42 @@ public class characterStats : MonoBehaviour
             alignmentIcons[0].SetActive(false);
             alignmentIcons[1].SetActive(true);
             alignmentIcons[3].SetActive(false);
-            if(byTheBook >= 2 && looseUnit <=1)
             mmFeedbacks.PlayFeedbacks();
         }
-
+        // Good Cop / Bad Cop Alignments
+        if (goodCop >= 2 && badCop <= 1 && aligned2 == true)
+        {
+            alignmentIcons[4].SetActive(true);
+            alignmentIcons[5].SetActive(false);
+            alignmentIcons[7].SetActive(true);
+            alignmentIcons[6].SetActive(false);
+            mmFeedbacks.PlayFeedbacks();
+        }
+        if (badCop >= 2 && goodCop <= 1 && aligned2 == true)
+        {
+            alignmentIcons[6].SetActive(true);
+            alignmentIcons[4].SetActive(false);
+            alignmentIcons[5].SetActive(true);
+            alignmentIcons[7].SetActive(false);
+            mmFeedbacks.PlayFeedbacks();
+        }
+        // Meat Head / Hacker Man Alignments
+        if (meatHead >= 2 && hackerMan <= 1 && aligned3 == true)
+        {
+            alignmentIcons[8].SetActive(true);
+            alignmentIcons[9].SetActive(false);
+            alignmentIcons[11].SetActive(true);
+            alignmentIcons[10].SetActive(false);
+            mmFeedbacks.PlayFeedbacks();
+        }
+        if (hackerMan >= 2 && meatHead <= 1 && aligned3 == true)
+        {
+            alignmentIcons[9].SetActive(true);
+            alignmentIcons[8].SetActive(false);
+            alignmentIcons[10].SetActive(true);
+            alignmentIcons[11].SetActive(false);
+            mmFeedbacks.PlayFeedbacks();
+        }
     }
     public void PlayerStats()
     {
@@ -173,5 +470,15 @@ public class characterStats : MonoBehaviour
         Debug.Log("Dayholt is: true = easily distracted , false = perceptive: " + pstat3);
         Debug.Log("Loose Unit is: " + looseUnit);
         Debug.Log("By The Book is: " + byTheBook);
+
+        Debug.Log("Dayholt is: true = Friendly , false = Cold: " + istat1);
+        Debug.Log("Dayholt is: true = Trusting , false = Intimidating: " + istat2);
+        Debug.Log("Dayholt is: true = Empathetic , false = Contrarian: " + istat3);
+        Debug.Log("Good Cop is: " + goodCop +" Bad Cop is: " + badCop);
+
+        Debug.Log("Dayholt is: true = Method Man , false = Integrated: " + intstat1);
+        Debug.Log("Dayholt is: true = Shredded , false = Slender: " + intstat2);
+        Debug.Log("Dayholt is: true = Wrench Monkey , false = Coded: " + intstat3);
+        Debug.Log("Meat Head is: " + meatHead + " Hacker Man is: " + hackerMan);
     }
 }
