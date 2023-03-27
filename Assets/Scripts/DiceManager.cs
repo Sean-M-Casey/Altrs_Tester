7d6f02b1382d431566dcac51c87c0f7be7e78691
Darcy Glover <67358033+darcyglover@users.noreply.github.com>
2023-03-27 14:30:19 +1100

    Rolling from spawn points properly set up and some bug fixes

--- Assets/Scripts/DiceManager.cs
+++ Assets/Scripts/DiceManager.cs
@@ -1,64 +1,64 @@
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DiceManager : MonoBehaviour
 {
     [SerializeField] Transform[] spawnPoints;
+    [SerializeField] Transform centrePoint;
     Transform dice;
 
-    //Camera main;
+    [SerializeField] float secondsBeforeDestroying;
 
-    [SerializeField] LayerMask diceColliderLayerMask/*, canPickupLayerMask*/;
+    [SerializeField] LayerMask diceColliderLayerMask;
 
     [Tooltip("Keep this at a length of 2! It will not work otherwise :)")]
     [SerializeField] int[] forceRange = new int[2];
 
     Rigidbody rigidBody;
 
-    //Ray ray;
+    bool diceRolled;
 
     void Awake()
     { 
-        //main = Camera.main;
+        foreach(var t in spawnPoints) //getting the same rotation for each spawnpoint to throw the dice in the same direction
+        {
+            t.LookAt(centrePoint);
+        }
     }
 
     void Update()
     {
-        if(rigidBody != null)
+        if(rigidBody != null && diceRolled)
         {
             if (rigidBody.velocity == Vector3.zero)
             {
                 OutputRoll();
             }
         }
-
-        //ray = main.ScreenPointToRay(Input.mousePosition);
-
-        //if(Physics.Raycast(ray, out RaycastHit hit, canPickupLayerMask))
-        //{
-        //    if (Input.GetMouseButton(0))
-        //    {
-        //        diceStats.transform.position = new Vector3(hit.point.x, 1, hit.point.z);
-
-        //        rigidBody.AddForce(hit.point);
-        //    }
-        //}
     }
 
-    public void ThrowDice(Transform dicePrefab)
+    public void ThrowDice(Transform dicePrefab) //this method takes in the dice prefab from the button, picks a random spawnpoint, then a random force amount, and then throws the instantiated dice prefab with that force.
     {
-        int random = Random.Range(0, spawnPoints.Length - 1);
+        int random = Random.Range(0, spawnPoints.Length);
 
-        dice = Instantiate(dicePrefab, spawnPoints[random].position, Quaternion.identity);
+        Transform pickedSpawnPoint = spawnPoints[random];
+
+        dice = Instantiate(dicePrefab, pickedSpawnPoint.position, Quaternion.identity);
 
         random = Random.Range(forceRange[0], forceRange[1]);
 
-        Debug.Log("Dice was thrown with a force of: " + random);
+        Debug.Log("Dice was thrown from: " + pickedSpawnPoint.name + ", with a force of: " + random);
+
+        diceRolled = false;
+
+        StartCoroutine(WaitForThrow());
 
         rigidBody = dice.GetComponent<Rigidbody>();
+        rigidBody.AddForce(pickedSpawnPoint.forward * random, ForceMode.Force);
 
-        rigidBody.AddForce(new Vector3(1, 0, 1) * random, ForceMode.Force);
+        StartCoroutine(DestroyAfterSeconds(dice.gameObject));
     }
 
     void OutputRoll()
@@ -67,5 +67,19 @@ public class DiceManager : MonoBehaviour
         {
             Debug.Log(hit.collider.name);
         }
+    }
+
+    IEnumerator DestroyAfterSeconds(GameObject diceToDestroy) //destroys dice after a certain time to reduce performance issues.
+    {
+        yield return new WaitForSeconds(secondsBeforeDestroying);
+
+        Destroy(diceToDestroy);
+    }
+
+    IEnumerator WaitForThrow() //without this, the dice returns 20 before it has a chance to apply force, leading to potential inaccuracies in the future.
+    {
+        yield return new WaitForSeconds(0.25f); //makes the update function wait a quarter of a second before outputting the result, i.e, waiting for the force to be applied.
+
+        diceRolled = true;
     }
 }
